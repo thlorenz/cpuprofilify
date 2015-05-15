09f0c07b67ca6c765c0621fd6ef8f8326795f390
phijohns <phillip.johnsen@finn.no>
2015-05-15 15:45:01 +0200

    Cleaning up stack frame names from Instruments as with perf and dtrace.
    Removes LazyCompile, Function and Script prefixes from stack names.

--- test/instruments.integration.js
+++ test/instruments.integration.js
@@ -7,7 +7,7 @@ if (typeof window !== 'undefined') return;
  * These tests check the entire JSON cpuprofiles generated via the conversion.
  * The data was manually check inside DevTools to confirm that it makes sense/is valid.
  *
- * Should the tests break (since they are brittle) please regenerate the cpuprofiles, check them very carefully 
+ * Should the tests break (since they are brittle) please regenerate the cpuprofiles, check them very carefully
  * manually and then overwrite the expected results in order to pass the tests again.
  */
 
@@ -22,6 +22,19 @@ test('\nInstruments default opts integration', function (t) {
   var expected = JSON.parse(expectedJSON);
 
   var res = cpuprofilify.convert(trace);
+
+  t.deepEqual(res, expected, 'matches previously generated cpuprofile exactly')
+  t.end()
+})
+
+test('\nInstruments with provided map-file integration', function (t) {
+  var trace = fs.readFileSync(__dirname + '/fixtures/instruments.unresolved.csv', 'utf8').split('\n')
+    , map = fs.readFileSync(__dirname + '/fixtures/instruments.unresolved.map', 'utf8')
+    , expectedJSON = fs.readFileSync(__dirname + '/fixtures/instruments.unresolved.cpuprofile', 'utf8')
+
+  var expected = JSON.parse(expectedJSON);
+
+  var res = cpuprofilify.convert(trace, { map: map });
 
   t.deepEqual(res, expected, 'matches previously generated cpuprofile exactly')
   t.end()
